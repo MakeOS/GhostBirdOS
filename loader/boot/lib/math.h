653d3dee0786928e37e05028911e6de1469de1d6
HuWenjie <1@hwj.me>
2018-07-12 17:01:18 +0800

    Add math funcions

---
+++ loader/boot/lib/math.h
@@ -0,0 +1,16 @@
+/**
+ * Copyright 2013-2015 by Explorer Developers.
+ * made by Lab Explorer Developers<1@hwj.me>
+ * Explorer Math Library Definition
+ * /loader/boot/lib/math.h
+ * version:Alpha
+ * 11/1/2014 12:09 AM
+ */
+
+#ifndef MATH_H_
+#define MATH_H_
+
+/**取一个有符号数的绝对值函数*/
+long abs(long number);
+
+#endif
