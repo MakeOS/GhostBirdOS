3667a7d91644a07c953ec0374fbf1b8ddb73fed0
HuWenjie <1@hwj.me>
2018-10-03 14:01:50 +0800

    types.h
    Assembly-style char/short/int/long long

---
+++ tools/gfr/include/types.h
@@ -0,0 +1,21 @@
+/**
+ * Date:2018/9/10 2:50
+ * Author:HuWenjie<1@hwj.me>
+ * Ghost Bird File System Reader
+ * Tyoes defination
+ * 
+ * BSD 2-Clause License
+ * 
+ * Copyright (c) 2017, Ghost Bird Operating System Project Developers.
+ * All rights reserved.
+ **/
+
+#ifndef TYPES_H_
+#define TYPES_H_
+
+typedef unsigned char		db;
+typedef unsigned short		dw;
+typedef unsigned int		dd;
+typedef unsigned long long	dq;
+
+#endif
