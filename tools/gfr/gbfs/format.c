5bf1339c31d1aeac32603f6ffc3728b7f4929a5c
HuWenjie <1@hwj.me>
2018-10-08 15:12:06 +0800

    finished format function

--- tools/gfr/gbfs/format.c
+++ tools/gfr/gbfs/format.c
@@ -11,38 +11,91 @@
  **/
 
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #include "../include/types.h"
 #include "../include/return.h"
 #include "../include/storage.h"
 #include "../include/gbfs.h"
-
-/**
-计算FAT、
-
-**/
+#include "../include/ui.h"
 
 GBFS *gbfs_fmt(Storage *sd, db partition)
 {
 	GBFS *gd = NULL;
-	
-	if(partition >= 4)
-	{
-		printf("Partition number cannot more than or equal 4.\n");
-		return NULL;
-	}
+	dd page_sctor;
+	dd b, n, m;
+	Page *root = NULL;
 	
 	printf("\nFormating partition[%d]...\n\n", partition);
 	
-	while (!gd) gd = malloc(sizeof(GBFS));
-	memset(gd, 0, sizeof(GBFS));
+	if (partition >= 4)
+	{
+		printf("\tPartition number cannot more than or equal 4.\n");
+		return NULL;
+	}
 	
-	sd->mbr->pt[partition];
+	if (!sd->mbr->pt[partition].size_s)
+	{
+		printf("\tThis partition does not exist.\n");
+		return NULL;
+	}
+	
+	while (!gd) gd = malloc(sizeof(GBFS));
+	memset(gd, 0, sizeof(GBFS));	
+	while (!gd->pbr) gd->pbr = malloc(sizeof(PBR));
+	while (!gd->fat) gd->fat = malloc(sizeof(Sector));
+	while (!root) root = malloc(sizeof(Page));
 	
 	gd->sd = sd;
 	gd->partition = partition;
 	
+	stg_gets(sd, STG_PBR_LBA(sd, partition), gd->pbr);
+	page_sctor = (sd->mbr->pt[partition].size_s - 1) \
+		* sizeof(Sector) / (sizeof(dd) + sizeof(Sector));
+	gd->pbr->fat_sctor = page_sctor \
+		* sizeof(dd) / sizeof(Sector) + 1;
+	gd->pbr->free_page = page_sctor - 2;
+	gd->pbr->first_page = 1;
+	sd->puts(sd, STG_PBR_LBA(sd, partition), gd->pbr);
 	
+	printf(
+		"\tpartition sector:%d\n"
+		"\tFAT sector:%d, page sector:%d\n"
+		, sd->mbr->pt[partition].size_s
+		, gd->pbr->fat_sctor, page_sctor
+	);
+	
+	ui_prog_start("Formating");
+	ui_prog_update(2, gd->pbr->fat_sctor + 3);
+	
+	// Build FAT
+	gd->fat->dword[0] = GBFS_FAT_END;
+	for (n = 1, m = 1; n < page_sctor;)
+	{
+		if (page_sctor - n > sizeof(Sector) / sizeof(dd))
+		{
+			b = sizeof(Sector) / sizeof(dd);
+			for (; m < b; m ++) gd->fat->dword[m] = ++ n;
+			m = 0;
+		}
+		else
+		{
+			b = page_sctor - n;
+			for (m = 0; m < b; m ++) gd->fat->dword[m] = ++ n;
+			gd->fat->dword[m - 1] = GBFS_FAT_END;
+			for (; m < sizeof(Sector) / sizeof(dd); m ++)
+				gd->fat->dword[m] = GBFS_FAT_NUL;
+		}
+		sd->puts(sd, GBFS_FAT_LBA(gd, (n - 1) / (sizeof(Sector) / sizeof(dd))), gd->fat);
+		ui_prog_update(1, 0);
+	}
+	
+	// Build root directory
+	for (n = 0; n < SECTOR_SIZE / sizeof(dq); n ++)
+		root->qword[n] = 0;
+	sd->puts(sd, GBFS_PAGE_LBA(gd, 0), root);
+	ui_prog_update(1, 0);
+	ui_prog_finish();
 	
 	return gd;
 }
